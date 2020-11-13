1a6688efd38bea4cdbc52a00171e922bf235b837
sdonohue0918 <sdonohue0918@gmail.com>
2020-11-12 19:45:00 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,35 @@
-// add code snippets from README
+// add code snippets from READMEv
+var state;
+
+function reducer(state = {count: 0}, action) {
+    switch (action.type) {
+        case "INCREASE_COUNT":
+            return {count: state.count + 1}
+        case "DECREASE_COUNT":
+            return {count: state.count - 1}
+        default:
+            return state
+
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state,action)
+    render()
+}
+// reducer(state, {type: 'INCREASE_COUNT'})
+
+function render() {
+    let cont = document.getElementById('container')
+    cont.textContent = state.count
+    
+   
+}
+
+var button = document.getElementById('button')
+
+button.onclick = () => {
+    dispatch({type: "INCREASE_COUNT"})
+}
+
+dispatch({type: '@@INIT'})
